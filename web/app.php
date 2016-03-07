a88885f317d5e9a413faabc8a404c21da6572092
Mishura Yuriy <Yuriy.Mishura@modera.net>
2016-03-07 18:36:56 +0200

    up

--- web/app.php
+++ web/app.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 use Symfony\Component\ClassLoader\ApcClassLoader;
-use Symfony\Component\HttpFoundation\Request;
-
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-=======
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -13,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
  */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
->>>>>>> bb42cde024c869398b3ab734d17cf8dbebc47ef7
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -29,12 +23,10 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
-<<<<<<< HEAD
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-=======
->>>>>>> bb42cde024c869398b3ab734d17cf8dbebc47ef7
+
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
--- web/app_dev.php
+++ web/app_dev.php
@@ -4,17 +4,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
-<<<<<<< HEAD
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-=======
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
->>>>>>> bb42cde024c869398b3ab734d17cf8dbebc47ef7
-//umask(0000);
+
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-<<<<<<< HEAD
+
 // if (isset($_SERVER['HTTP_CLIENT_IP'])
 //     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
 //     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
@@ -28,14 +22,6 @@ Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-=======
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
@@ -43,7 +29,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
->>>>>>> bb42cde024c869398b3ab734d17cf8dbebc47ef7
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
